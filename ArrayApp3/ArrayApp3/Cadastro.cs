2aaa4155bce571fd6c800ea22a401337355f33a1
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-09-10 19:45:41 -0300

    Exercício num 3 de vetores

---
+++ ArrayApp3/ArrayApp3/Cadastro.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace ArrayApp3
+{
+    class Cadastro
+    {
+        public string ClientName { get; set; }
+        public string Email { get; set; }
+        public int RoomNumber { get; set; }
+
+        public Cadastro ()
+        {
+        }
+
+        public Cadastro( string nome, string email, int number)
+        {
+            ClientName = nome;
+            Email = email;
+            RoomNumber = number;
+        }
+
+        
+        public override string ToString()
+        {
+            return "Nome: " + ClientName + "; email: " + Email + "; Quarto: " + RoomNumber;
+        }
+    }
+}
---
+++ ArrayApp3/ArrayApp3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ArrayApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cadastro[] rooms = new Cadastro[10];
+            int counter = 0;
+            Console.WriteLine("Olá! Quantos quartos gostaria de alugar?");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vamos iniciar os cadastros");
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.WriteLine($"Cadastro {i + 1}");
+
+                Console.Write("Insira o nome: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Insira o email: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Insira o número do quarto desejado: ");
+                int number = int.Parse(Console.ReadLine());
+
+                rooms[i] = new Cadastro { ClientName = name, Email = email, RoomNumber = number };
+                if (counter != 0)
+                {
+
+
+                    //checa se o quarto está ocupado
+                    for (int j = 0; j < counter; j++)
+                    {
+                        if (rooms[j].RoomNumber == number)
+                        {
+                            Console.WriteLine("Quarto ocupado! Por favor, escolha outro");
+                            number = int.Parse(Console.ReadLine());
+                            rooms[i] = new Cadastro { ClientName = name, Email = email, RoomNumber = number};
+                            j = -1;
+                        }
+                    }
+
+
+
+                }
+                counter++;
+                Console.WriteLine("Quarto reservado com sucesso.");
+            }
+
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("Reservas feitas: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if(rooms[i] != null)
+                {
+                    Console.WriteLine(i + ": " + rooms[i]);
+                }
+            }
+            Console.WriteLine("----------------------------------------------------");
+
+
+        }
+
+    }
+}
