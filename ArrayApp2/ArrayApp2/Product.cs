d273821cf987508c860c5c6a79678264418b49c4
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-09-10 14:34:51 -0300

    Exercícios com array

---
+++ ArrayApp2/ArrayApp2/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayApp2
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+    }
+}
---
+++ ArrayApp2/ArrayApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace ArrayApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira a quantidade de produtos: ");
+            int n = int.Parse(Console.ReadLine());
+            double sum = 0.0;
+            Product[] v = new Product[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("Digite o nome do produto " + (i+1) + ": ");
+                string name = Console.ReadLine();
+                Console.Write("Digite o preço do produto " + (i+1) + ": ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                v[i] = new Product{ Name = name, Price = price };
+
+                sum += v[i].Price;
+            }
+            double avg = sum / n;
+            Console.Write("A média dos preços dos produtos é: ");
+            Console.WriteLine(avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
