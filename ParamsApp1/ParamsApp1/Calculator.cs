f5de411a64998d05a1b08cb3c2d4fd73ad9b086c
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-09-11 19:21:38 -0300

    Exercício com uso do modificador params

---
+++ ParamsApp1/ParamsApp1/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParamsApp1 {
+    class Calculator {
+        public static double Sum(params double[] numbers) {
+            double sum = 0.00;
+            for(int i = 0; i < numbers.Length; i++) {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ ParamsApp1/ParamsApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+
+namespace ParamsApp1 {
+    class Program {
+        static void Main(string[] args) {
+            //sem o params: double result = Calculator.Sum(new int[] {1, 2});
+            double result = Calculator.Sum(1.5, 2.7);
+            Console.WriteLine("result = " + result.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
