17af6b586626fc03e0efc62c5fc143f56f240ede
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-09-12 01:20:19 -0300

    Exemplo de uso do foreach

---
+++ ForEachApp1/ForEachApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ForEachApp1 {
+    class Program {
+        static void Main(string[] args) {
+            //criar o vetor
+            string[] vect = new string[] { "Maria", "Bob", "Alex" };
+            //para cada objeto em vect, fazer tal coisa
+            foreach(string obj in vect) {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
