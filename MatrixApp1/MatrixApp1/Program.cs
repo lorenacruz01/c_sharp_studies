f76f151443326a659d6757280a6c126477eb8484
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-09-13 12:31:05 -0300

    Aula inicial matriz

---
+++ MatrixApp1/MatrixApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MatrixApp1 {
+    class Program {
+        static void Main(string[] args) {
+
+            int linhas = 2;
+            int colunas = 3;
+            double[,] mat = new double[linhas, colunas];
+
+            //quantidade de elementos (tamanho da matriz)
+            Console.WriteLine("A matriz possui " + mat.Length + " elementos");
+
+            //quantidade de linhas
+            Console.WriteLine("A matriz possui " + mat.Rank + " linhas"); //poderia usar mat.GetLength(0)
+            Console.WriteLine("A matriz possui " + mat.GetLength(1) + " colunas");
+
+        }
+    }
+}
