a31b1f396ff286bdad098735b970b5a524ca12de
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-09-15 23:54:33 -0300

    Exercícios de composição e enumeração

---
+++ CompositionApp1/CompositionApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using CompositionApp1.Entities;
+using CompositionApp1.Entities.Enums;
+
+namespace CompositionApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o nome do departamento: ");
+            string deptName = Console.ReadLine();
+            Console.WriteLine("Preecha os dados a seguir.");
+            Console.Write("Nome do funcionário: ");
+            string name = Console.ReadLine();
+            Console.Write("Nível de experiência (Junior/Midlevel/Senior)");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Salário base: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(deptName);
+            Worker worker = new Worker(name, level, baseSalary, dept);
+            Console.WriteLine("Funcionário cadastrado com sucesso!");
+            Console.WriteLine("-----------------------------------");
+            Console.Write("Quantos contratos serão cadastrados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Entre com os dados do contrato {i}");
+                Console.Write("Data DD/MM/YYYY: ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Valor por hora: ");
+                double valuePerHour = double.Parse(Console.ReadLine());
+                Console.Write("Duração (horas): ");
+                int hours = int.Parse(Console.ReadLine());
+
+                HourContract contract = new HourContract(date, valuePerHour, hours);
+                worker.AddContract(contract);
+            }
+
+            
+
+
+
+        }
+    }
+}
