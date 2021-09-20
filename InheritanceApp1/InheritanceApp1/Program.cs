ffe4e0e3a5ea9c3fff7f0a8ac94f77a57873f70f
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-09-20 12:24:58 -0300

    Exercício inicial de herança

---
+++ InheritanceApp1/InheritanceApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using InheritanceApp1.Entities;
+
+namespace InheritanceApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Alex Von Hohenheim", 100.0, 500);
+            Console.WriteLine(account.Balance);
+
+            account.Withdraw(10.5);
+
+            Console.WriteLine(account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
