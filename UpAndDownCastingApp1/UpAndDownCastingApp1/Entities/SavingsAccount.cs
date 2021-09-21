3633bf2fc75d5c808db491442465018a2c0f1be9
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-09-21 16:56:44 -0300

    Exercício de Upcasting e Downcasting

---
+++ UpAndDownCastingApp1/UpAndDownCastingApp1/Entities/SavingsAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpAndDownCastingApp1.Entities
+{
+    class SavingsAccount : Account 
+    { 
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance = Balance * InterestRate;
+        }
+
+
+    }
+}
---
+++ UpAndDownCastingApp1/UpAndDownCastingApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using UpAndDownCastingApp1.Entities;
+
+namespace UpAndDownCastingApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* BusinessAccount account = new BusinessAccount(8010, "Alex Von Hohenheim", 100.0, 500);
+             Console.WriteLine(account.Balance);
+
+             account.Withdraw(10.5);
+
+             Console.WriteLine(account.Balance.ToString("F2", CultureInfo.InvariantCulture));*/
+
+            Account acc = new Account(1050, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1051, "Maria", 0.0, 500.00);
+
+            //UPCASTING (conversão da subclasse para superclasse)
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1235, "Bob", 0.0, 200);
+            Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+
+            //DOWNCASTING (conversão da superclasse para subclasse)
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.00);
+
+            // uso do is
+            if(acc is BusinessAccount)
+            {
+                BusinessAccount acc5 = (BusinessAccount)acc3;
+                acc5.Loan(100.00);
+                Console.WriteLine("Empréstimo feito com sucesso!");
+            }
+
+            if(acc3 is SavingsAccount)
+            {
+                SavingsAccount acc5 = (SavingsAccount)acc3;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update");
+            }
+
+        }
+    }
+}
