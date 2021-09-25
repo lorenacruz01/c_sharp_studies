df7bce06ad1de826fd7b78dfc4e2f958f21488c5
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-09-25 00:31:02 -0300

    Exercício de sobrescrita. Uso do override, virtual e base

---
+++ VirtualOverrideBaseApp1/VirtualOverrideBaseApp1/Entities/BusinessAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualOverrideBaseApp1.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+
+        //exemplo utilizando o base na sobrescrita
+        public override void Withdraw(double amount)
+        {
+            base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}
