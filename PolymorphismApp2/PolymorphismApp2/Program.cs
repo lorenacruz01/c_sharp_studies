cf72d31fad4a04d5f15d89b674a24710ef88a500
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-10-12 01:22:34 -0300

    Exercício 2 de polimorfismo

---
+++ PolymorphismApp2/PolymorphismApp2/Program.cs
@@ -0,0 +1,50 @@
+﻿using PolymorphismApp2.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace PolymorphismApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>(); 
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i=1; i<=n; i++)
+            {
+                Console.WriteLine($"Product #{i} data");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char type = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (type == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    products.Add(new UsedProduct(name, price, date));
+                } else if (type == 'i')
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    products.Add(new ImportedProduct(name, price, customsFee));
+                }
+                else
+                {
+                    products.Add(new Product(name, price));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS: ");
+            foreach(Product item in products)
+            {
+                Console.WriteLine(item.PriceTag());
+            }
+        }
+    }
+}
