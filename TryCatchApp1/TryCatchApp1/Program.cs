8291fc8f998c75c31aa2fb8888984eb44687b68e
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-10-17 19:33:36 -0300

    Exercício de exceções com estrutura try-catch

---
+++ TryCatchApp1/TryCatchApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace TryCatchApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Digite o primeiro número: ");
+                int n1 = int.Parse(Console.ReadLine());
+                Console.Write("Digite o segundo número: ");
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+
+                Console.WriteLine("Resultado: " + result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Não é permitido divisão por zero");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Erro! O input deve ser numérico");
+            }
+           
+        }
+    }
+}
