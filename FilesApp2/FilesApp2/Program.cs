dc234a107d55bd1fcc4a794b28617a9ad556ad84
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-11-25 14:40:09 -0300

    Utilização do bloco using

--- FilesApp2/FilesApp2/Program.cs
+++ FilesApp2/FilesApp2/Program.cs
@@ -8,30 +8,28 @@ namespace FilesApp2
         static void Main(string[] args)
         {
             string path = @"C:\workspace\c_sharp_studies\FilesApp2\file.txt";
-            //FileStream fs = null;
-            StreamReader sr = null;
+
             try
             {
-                //fs = new FileStream(path, FileMode.Open);
-                //sr = new StreamReader(fs);
-                sr = File.OpenText(path);
-                while(!sr.EndOfStream)
+                using (FileStream fs = new FileStream(path, FileMode.Open))
                 {
-                    string line = sr.ReadLine();
-                    Console.WriteLine(line);
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
                 }
-                
             }
-            catch(IOException e)
+            catch (IOException e)
             {
                 Console.WriteLine("An error has ocurred!");
                 Console.WriteLine(e.Message);
             }
-            finally
-            {
-                if (sr != null) sr.Close();
-                //if (fs != null) fs.Close();
-            }
+
+            
 
         }
     }
