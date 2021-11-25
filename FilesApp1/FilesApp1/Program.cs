736d2b4ae8ffc1fc26c013a241a3df4fd87d3236
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-11-25 13:13:57 -0300

    File, FileInfo e IOException

---
+++ FilesApp1/FilesApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace FilesApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\workspace\c_sharp_studies\FilesApp1\csharpfiletest.txt";
+            string targetPath = @"C:\workspace\c_sharp_studies\FilesApp1\csharpfiletest2.txt";
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
