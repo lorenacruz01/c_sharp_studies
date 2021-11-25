f36d5fd45ee5005d0d37bb235c86b447339447cd
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-11-25 15:32:57 -0300

    Uso do StreamWriter

---
+++ FilesApp3/FilesApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace FilesApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\workspace\c_sharp_studies\FilesApp3\file.txt";
+            string targetPath = @"C:\workspace\c_sharp_studies\FilesApp3\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error has ocurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
