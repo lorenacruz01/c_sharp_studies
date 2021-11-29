dcbcb45ebb7dc6927b16d4dfe75cc3618d4bfa8b
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-11-29 12:57:02 -0300

    Exemplo sem interface

---
+++ InterfaceApp1/Entities/Invoice.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace InterfaceApp1.Entities
+{
+    class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        //propriedade calculada
+        public double TotalPayment
+        {
+            get { return BasicPayment + Tax; }
+        }
+        public override string ToString()
+        {
+            return "Basic Payment: " +
+                BasicPayment.ToString("F2", CultureInfo.InvariantCulture) +
+                "\nTax: " + Tax.ToString("F2", CultureInfo.InvariantCulture) +
+                "\nTotal Payment: " + TotalPayment.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ InterfaceApp1/Services/RentalService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using InterfaceApp1.Entities;
+
+
+namespace InterfaceApp1.Services
+{
+    class RentalService
+    {
+        public double PricePerHour { get; set; }
+        public double PricePerDay { get; set; }
+
+        private BrazilTaxService _brazilTaxService = new BrazilTaxService();
+
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish - carRental.Start;
+            double basicPayment = 0.0;
+            if (duration.TotalHours <= 12.0)
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+
+            double tax = _brazilTaxService.Tax(basicPayment);
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
+    }
+}
