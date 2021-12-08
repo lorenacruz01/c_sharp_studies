80ac9ead10251909a5cce3919920d8af84a72d48
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-12-08 02:14:10 -0300

    Uso da interface IComparable

---
+++ IComparableApp1/IComparableApp1/Entities/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace IComparableApp1.Entities
+{
+    class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvEmployee)
+        {
+            string[] v = csvEmployee.Split(',');
+            Name = v[0];
+            Salary = double.Parse(v[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + ", Salary: " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Employee))
+            {
+                throw new ArgumentException("Comparing error: argumento is not an Employee");
+            }
+            Employee other = obj as Employee;
+            return Salary.CompareTo(other.Salary);
+        }
+    }
+}
---
+++ IComparableApp1/IComparableApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using IComparableApp1.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IComparableApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\workspace\c_sharp_studies\IComparableApp1\teste.txt";
+            Console.WriteLine("teste");
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach(Employee str in list)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+            } catch(IOException e)
+            {
+                Console.WriteLine("An error has ocurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
