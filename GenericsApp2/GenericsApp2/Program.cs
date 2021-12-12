b5458dfc5fed2246ec32ad395f228232a89546b8
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-12-11 23:02:07 -0300

    Restrições de generics (continuação)

---
+++ GenericsApp2/GenericsApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using GenericsApp2.Services;
+using System;
+using System.Collections.Generic;
+
+namespace GenericsApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Console.Write("Enter n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+
+            CalculationService calculationService = new CalculationService();
+            int max = calculationService.Max(list);
+            Console.WriteLine("Max: " + max);
+            
+        }
+    }
+}
---
+++ GenericsApp2/GenericsApp2/Services/CalculationService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsApp2.Services
+{
+    class CalculationService
+    {
+        public int Max(List<int> list)
+        {
+            if(list.Count == 0)
+            {
+                throw new ArgumentException("The list cannot be empty!");
+            }
+
+            int max = list[0];
+            for(int i = 0; i < list.Count; i++)
+            {
+                if(list[i] > max)
+                {
+                    max = list[i];
+                }
+            }
+            return max;
+        }
+    }
+}
