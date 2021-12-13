e643d5c0920103951f3e6925000fba52fcdab889
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-12-13 15:36:07 -0300

    Aula GetHashCode e Equals - uso do Equals

---
+++ EqualsApp1/EqualsApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EqualsApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Alex";
+            string b = "Maria";
+            string c = "Maria";
+            Console.WriteLine("A and B => Equal?");
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine("B and C => Equal?");
+            Console.WriteLine(b.Equals(c));
+        }
+    }
+}
