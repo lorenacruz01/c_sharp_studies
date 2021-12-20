dceb48746d38a9a36af22fcbb2cbed4d6c8cf915
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-12-20 12:45:03 -0300

    Aula de HashSet e SortedSet: uso do SortedSet

---
+++ HashSetAndSortedSet/HashSetAndSortedSet/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSetAndSortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Uso do HashSet---");
+            HashSet<string> set = new HashSet<string>();
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("tablet");
+            Console.WriteLine(set.Contains("Notebook"));
+
+
+            foreach (string s in set)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("---Uso do SortedSet---");
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+            Console.Write("a: ");
+            PrintCollection(a);
+            Console.Write("b: ");
+            PrintCollection(b);
+            //union
+            Console.WriteLine("---Uso do UnionWith---");
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+            PrintCollection(c);
+
+            //intersection
+            Console.WriteLine("---Uso do IntersectWith---");
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            PrintCollection(d);
+
+            //difference
+            Console.WriteLine("---Uso do ExceptWith---");
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            PrintCollection(e);
+        }
+
+        static void PrintCollection<T> (IEnumerable<T> collection)
+        {
+            foreach(T obj in collection)
+            {
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
