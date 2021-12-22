90834f31440647c03b6e4d1ffb80f430fef42ee4
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-12-22 01:02:56 -0300

    Como as coleções hash comparam igualdade

---
+++ HashSetSortedSetApp2/HashSetSortedSetApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using HashSetSortedSetApp2.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace HashSetSortedSetApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Product> a = new HashSet<Product>();
+            a.Add(new Product("TV", 900.00));
+            a.Add(new Product("Notebook", 1200.00));
+
+            HashSet<Point> b = new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
+
+            Product prod = new Product("Notebook", 1200.00);
+            Console.WriteLine("a contains prod? " + a.Contains(prod));
+
+            Point p = new Point(5, 10);
+            Console.WriteLine( "b contains p? " + b.Contains(p));
+        }
+    }
+}
