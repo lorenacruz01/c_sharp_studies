bf852ccbdaaffc04ea21d9349930fb20d931c6d3
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2021-12-27 13:55:42 -0300

    Aula de Dictionary e SortedDictionary

---
+++ DictionaryApp1/DictionaryApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+            cookies["user"] = "maria";
+            cookies["email"] = "mariateste@gmail.com";
+            cookies["phone"] = "11111111";
+            cookies["phone"] = "22222222"; //não admite repetições na mesma chave, então sobrescreve
+
+            Console.WriteLine(cookies["user"]);
+            Console.WriteLine(cookies["email"]);
+            Console.WriteLine(cookies["phone"]);
+
+            cookies.Remove("email");
+
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            } else
+            {
+                Console.WriteLine("There is no 'email' key!");
+            }
+
+            Console.WriteLine("Dictionary's size: " + cookies.Count);
+            Console.WriteLine("---ALL COOKIES---");
+            foreach(KeyValuePair<string, string> item in cookies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+    }
+}
