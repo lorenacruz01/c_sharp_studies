dbb7080813e29d8a058a48a99f9a6d31a9b9a120
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2022-01-11 19:52:26 -0300

    Multicast delegates

---
+++ MulticastDelegatesApp1/MulticastDelegatesApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using MulticastDelegatesApp1.Services;
+using System;
+
+namespace MulticastDelegatesApp1
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 15;
+            double b = 17;
+
+            BinaryNumericOperation op = CalculationServices.ShowSum;
+            op += CalculationServices.ShowMax;
+            op.Invoke(a, b);
+        }
+    }
+}
---
+++ MulticastDelegatesApp1/MulticastDelegatesApp1/Services/CalculationServices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MulticastDelegatesApp1.Services
+{
+    class CalculationServices
+    {
+        public static void ShowMax(double x, double y)
+        {
+            double max = (x > y) ? x : y;
+            Console.WriteLine(max);
+        }
+
+        public static void ShowSum(double x, double y)
+        {
+            double result = x + y;
+            Console.WriteLine(result);
+        }
+    }
+}
