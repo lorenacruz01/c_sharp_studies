e8c38aaf8424ae1e6f797267165001167a5046b7
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2022-01-11 13:05:31 -0300

    Aula de delegates

---
+++ DelegatesApp1/DelegatesApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using DelegatesApp1.Services;
+using System;
+
+namespace DelegatesApp1
+{
+    class Program
+    {
+        delegate double BinaryNumericOperation(double n1, double n2); //especifica uma referência para uma função que recebe dois numeros double e retorna um double
+
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationServices.Sum;
+
+            //double result = op(a, b);
+            double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
