7634269e7c4be10f5089cee8773c599ccba77f7a
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2022-01-13 15:09:07 -0300

    Início das aulas sobre Linq

--- FuncApp1/FuncApp1/Program.cs
+++ FuncApp1/FuncApp1/Program.cs
@@ -16,18 +16,18 @@ namespace FuncApp1
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            Func<Product, string> func = NameUpper;
-            List<string> result = list.Select(func).ToList();
+            //Func<Product, string> func = NameUpper;
+            //Func<Product, string> func = p => p.Name.ToUpper();
+            List<string> result = list.Select(p => p.Name.ToUpper()).ToList();
             foreach(string s in result)
             {
                 Console.WriteLine(s);
             }
-
         }
 
-        static string NameUpper(Product p)
+        /*static string NameUpper(Product p)
         {
             return p.Name.ToUpper();
-        }
+        }*/
     }
 }
---
+++ LinqApp1/LinqApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //especificar data source
+            int[] numbers = new int[] { 2, 3, 4, 5, 6 };
+
+            //definir expressão de consulta
+            var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            //executar a consulta
+            foreach(int x in result)
+            {
+                Console.WriteLine(x);
+            }
+
+
+        }
+    }
+}
