cb27a9f59c4b521491559d050315f2d45bb1129b
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2022-01-13 01:10:33 -0300

    Aula sobre Func delegate - parte 1

---
+++ FuncApp1/FuncApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using FuncApp1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FuncApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            List<string> result = list.Select(NameUpper).ToList();
+            foreach(string s in result)
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+
+        static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
+        }
+    }
+}
