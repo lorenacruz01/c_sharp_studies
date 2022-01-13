0d51f308b3386f44b35a519cd57f0adefc4787d3
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2022-01-13 00:09:15 -0300

    Aula sobre o Action delegate

---
+++ ActionApp1/ActionApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using ActionApp1.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace ActionApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            //list.ForEach(UpdatePrice);
+            Action<Product> act = UpdatePrice;
+            list.ForEach(act);
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.10;
+        }
+    }
+}
